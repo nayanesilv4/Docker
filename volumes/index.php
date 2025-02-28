a035d1db370f36bb1ad6011ee648bddc31c9f10c
Nayane da Silva <nayanecosilva@gmail.com>
2025-02-28 08:25:53 -0300

    Add files via upload

---
+++ volumes/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mensagens</title>
+</head>
+
+<body>
+    <h1>Escreva sua mensagem! BIND MOUNT</h1>
+    <form action="process.php" method="POST">
+        <input type="text" name="message" id="message">
+        <input type="submit" value="Enviar Mensagem">
+    </form>
+</body>
+
+</html>
